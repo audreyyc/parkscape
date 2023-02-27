b2cadcb8019b6c9ba43d7ce03edfa27469b6cbd3
Pawan K Somavarpu <pawanksomavarpu@utexas.edu>
2023-02-26 18:03:14 -0600

    Installed React, created project structure, created Navigation

---
+++ frontend/src/pages/ParkInstance/ParkInstance.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function ParkInstance() {
+    return (
+      <div className="ParkInstance">
+        <h1>Park Instance</h1>
+      </div>
+    );
+  }
+  
+  export default ParkInstance;
