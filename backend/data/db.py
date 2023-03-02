cb821542162f56d869e6642420548f676293a14c
Petar Iliev <petariliev@utexas.edu>
2023-03-02 11:49:17 -0600

    Improve build files

--- backend/data/db.py
+++ backend/data/db.py
@@ -18,12 +18,6 @@ def setupDatabase():
     conn = engine.connect()
     metadata = db.MetaData()
 
-
-    # conn.set_character_set('utf8')
-    # dbc.execute('SET NAMES utf8;')
-    # dbc.execute('SET CHARACTER SET utf8;')
-    # dbc.execute('SET character_set_connection=utf8;')
-
     Cities = Table('cities', metadata,
               Column('id', Integer, primary_key=True),
               Column('airbnb_listings', String(80)),
