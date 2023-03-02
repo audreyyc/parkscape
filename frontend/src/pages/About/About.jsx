f1c0130973816e57cbc5309893ab1d764c6a8c38
Pawan K Somavarpu <pawanksomavarpu@utexas.edu>
2023-03-02 13:31:08 -0600

    dynamic pagination using API for cities

--- frontend/src/pages/About/About.jsx
+++ frontend/src/pages/About/About.jsx
@@ -1,17 +1,17 @@
-import React, { useState, useEffect } from 'react';
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
+import React, { useState, useEffect } from "react";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
 // import NavBar from './navbar'; // TODO: needs to be updated
-import DeveloperCard from '././components/DeveloperCard';
-import { DeveloperInfo } from './static-data/developer_info.js';
-import ToolCard from '././components/ToolCard';
-import { ToolsInfo } from './static-data/tools_info';
-import { APIInfo } from './static-data/api_info';
+import DeveloperCard from "../../components/DeveloperCard/DeveloperCard.jsx";
+import { DeveloperInfo } from "./static-data/developer_info.js";
+import ToolCard from "../../components/ToolCard/ToolCard.jsx";
+import { ToolsInfo } from "./static-data/tools_info";
+import { APIInfo } from "./static-data/api_info";
 
-import 'bootstrap/dist/css/bootstrap.css';
+import "bootstrap/dist/css/bootstrap.css";
 
 function AboutPage() {
   //* GitLab API calls *//
@@ -21,17 +21,18 @@ function AboutPage() {
   // Get commits per user
   async function get_commits() {
     var request = new XMLHttpRequest();
-    var url = 'https://gitlab.com/api/v4/projects/43345825/repository/contributors/';
-    request.open('GET', url);
-    request.onload = function() {
-        var result = JSON.parse(this.response);
-        result.forEach((data) => {
-          DeveloperInfo.forEach((user) => {
-            if (data.name === user.name) {
-              user.commits += data.commits; // TODO: needs to be fixed after commit history is fixed
-            }
-          })
-        })
+    var url =
+      "https://gitlab.com/api/v4/projects/43345825/repository/contributors/";
+    request.open("GET", url);
+    request.onload = function () {
+      var result = JSON.parse(this.response);
+      result.forEach((data) => {
+        DeveloperInfo.forEach((user) => {
+          if (data.name === user.name) {
+            user.commits += data.commits; // TODO: needs to be fixed after commit history is fixed
+          }
+        });
+      });
     };
     request.send();
   }
@@ -40,26 +41,30 @@ function AboutPage() {
   async function get_issues() {
     DeveloperInfo.forEach((user) => {
       var request = new XMLHttpRequest();
-      var url = 'https://gitlab.com/api/v4/projects/43345825/issues_statistics?author_id='.concat(user.user_id);
-      request.open('GET', url);
-      request.onload = function() {
-          var result = JSON.parse(this.response);
-          var num = result.statistics.counts.all;
-          user.issues = num;
+      var url =
+        "https://gitlab.com/api/v4/projects/43345825/issues_statistics?author_id=".concat(
+          user.user_id
+        );
+      request.open("GET", url);
+      request.onload = function () {
+        var result = JSON.parse(this.response);
+        var num = result.statistics.counts.all;
+        user.issues = num;
       };
       request.send();
-    })
+    });
   }
 
   // Get total commits
   async function total_commits() {
     var request = new XMLHttpRequest();
-    var url = 'https://gitlab.com/api/v4/projects/43345825/repository/commits/?per_page=1000';
-    request.open('GET', url);
-    request.onload = function() {
-        var result = JSON.parse(this.response);
-        var num = result.length;
-        setTotalCommits(num);
+    var url =
+      "https://gitlab.com/api/v4/projects/43345825/repository/commits/?per_page=1000";
+    request.open("GET", url);
+    request.onload = function () {
+      var result = JSON.parse(this.response);
+      var num = result.length;
+      setTotalCommits(num);
     };
     request.send();
   }
@@ -67,12 +72,12 @@ function AboutPage() {
   // Get total issues
   async function total_issues() {
     var request = new XMLHttpRequest();
-    var url = 'https://gitlab.com/api/v4/projects/43345825/issues_statistics';
-    request.open('GET', url);
-    request.onload = function() {
-        var result = JSON.parse(this.response);
-        var num = result.statistics.counts.all;
-        setTotalIssues(num);
+    var url = "https://gitlab.com/api/v4/projects/43345825/issues_statistics";
+    request.open("GET", url);
+    request.onload = function () {
+      var result = JSON.parse(this.response);
+      var num = result.statistics.counts.all;
+      setTotalIssues(num);
     };
     request.send();
   }
@@ -82,7 +87,7 @@ function AboutPage() {
     var total = 0;
     DeveloperInfo.forEach((user) => {
       total += user.unittests;
-    })
+    });
     return total;
   }
 
@@ -91,8 +96,8 @@ function AboutPage() {
     get_issues();
     total_commits();
     total_issues();
-    }, [])
-  
+  }, []);
+
   return (
     <>
       {/* Navbar */}
@@ -106,12 +111,13 @@ function AboutPage() {
         <p>
           ParkScape compiles information about United States airports, nearby
           cities, and their local state/national parks -- helping travelers who
-          are visiting specific cities or parks to find other nearby locations 
+          are visiting specific cities or parks to find other nearby locations
           of interest (specifically nearby cities, parks, and airports).
         </p>
         <p>
           Our purpose is to help travellers make more educated decisions about
-          their trips while encouraging them to experience the beautiful parks and cities of the US.
+          their trips while encouraging them to experience the beautiful parks
+          and cities of the US.
         </p>
       </Container>
 
@@ -121,13 +127,13 @@ function AboutPage() {
       </Container>
       <Container class="container text-center mt-5 mb-4">
         <Row class="row row-cols-md-5 g-4">
-        {DeveloperInfo.map((user) => {
-          return (
-            <Col class="col">
-              <DeveloperCard {...user}></DeveloperCard>
-            </Col>
-          )
-        })}
+          {DeveloperInfo.map((user) => {
+            return (
+              <Col class="col">
+                <DeveloperCard {...user}></DeveloperCard>
+              </Col>
+            );
+          })}
         </Row>
       </Container>
 
@@ -153,23 +159,23 @@ function AboutPage() {
       </Container>
       <Container class="container text-center mt-5 mb-4">
         <Row class="row row-cols-md-4 g-3">
-        {ToolsInfo.slice(0,4).map((tool) => {
-          return (
-            <Col class="col">
-              <ToolCard {...tool}></ToolCard>
-            </Col>
-          )
-        })}
+          {ToolsInfo.slice(0, 4).map((tool) => {
+            return (
+              <Col class="col">
+                <ToolCard {...tool}></ToolCard>
+              </Col>
+            );
+          })}
         </Row>
         <br></br>
         <Row class="row row-cols-md-4 g-3">
-        {ToolsInfo.slice(4).map((tool) => {
-          return (
-            <Col class="col">
-              <ToolCard {...tool}></ToolCard>
-            </Col>
-          )
-        })}
+          {ToolsInfo.slice(4).map((tool) => {
+            return (
+              <Col class="col">
+                <ToolCard {...tool}></ToolCard>
+              </Col>
+            );
+          })}
         </Row>
       </Container>
 
@@ -179,17 +185,19 @@ function AboutPage() {
       </Container>
       <Container class="container text-center mt-5 mb-4">
         <Row class="row row-cols-md-5 g-3">
-        {APIInfo.map((api) => {
-          return (
-            <Col class="col">
-              <ToolCard {...api}></ToolCard>
-            </Col>
-          )
-        })}
+          {APIInfo.map((api) => {
+            return (
+              <Col class="col">
+                <ToolCard {...api}></ToolCard>
+              </Col>
+            );
+          })}
         </Row>
       </Container>
       <Container class="container text-center mt-5 mb-5">
-        <a href="https://documenter.getpostman.com/view/25781480/2s935uGgU4"><Button variant="dark">Our API</Button></a>
+        <a href="https://documenter.getpostman.com/view/25781480/2s935uGgU4">
+          <Button variant="dark">Our API</Button>
+        </a>
       </Container>
     </>
   );
