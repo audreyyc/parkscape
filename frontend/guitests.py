51dba0d5ebf4b206e99a6cfbf1d11ef02510d496
Petar Iliev <petariliev@utexas.edu>
2023-03-06 12:16:40 -0600

    Update guitest.py

--- frontend/guitests.py
+++ frontend/guitests.py
@@ -16,6 +16,7 @@ class Test(unittest.TestCase):
         chrome_options.add_argument("--start-maximized")
         chrome_options.add_argument('--headless')
         chrome_options.add_argument('--no-sandbox')
+        chrome_options.add_argument("--disable-dev-shm-usage")
         
         self.driver = webdriver.Chrome(chrome_options=chrome_options)
         self.driver = webdriver.Chrome()
