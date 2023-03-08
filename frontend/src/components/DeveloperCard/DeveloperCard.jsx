22fd4bda9203b36f0838b1ad9c1e59517952946d
Pawan K Somavarpu <pawanksomavarpu@utexas.edu>
2023-03-08 14:29:00 -0600

    sample tests

--- frontend/src/components/DeveloperCard/DeveloperCard.jsx
+++ frontend/src/components/DeveloperCard/DeveloperCard.jsx
@@ -1,28 +1,34 @@
-import React from 'react';
-import Card from 'react-bootstrap/Card';
+import React from "react";
+import Card from "react-bootstrap/Card";
 
-function DeveloperCard (props) {
+function DeveloperCard(props) {
   return (
     <Card className="card h-100">
-        <Card.Img variant="top" src={props.image}></Card.Img>
-        <Card.Body>
-            <Card.Title><h5>{props.name}</h5></Card.Title>
-            <Card.Text><h6>@{props.user}</h6></Card.Text>
-            <Card.Text><h6>{props.role}</h6></Card.Text>
-            <Card.Text>{props.bio}</Card.Text>
-        </Card.Body>
-        <Card.Footer> 
-            <small class="text-muted">Commits: </small>
-            <small>{props.commits}</small>
-            <br></br>
-            <small class="text-muted">Issues: </small>
-            <small>{props.issues}</small>
-            <br></br>
-            <small class="text-muted">Unit Tests: </small>
-            <small>{props.unittests}</small>
-        </Card.Footer>
+      <Card.Img variant="top" src={props.image}></Card.Img>
+      <Card.Body>
+        <Card.Title>
+          <h5>{props.name}</h5>
+        </Card.Title>
+        <h6>
+          <Card.Text>@{props.user}</Card.Text>
+        </h6>
+        <h6>
+          <Card.Text>{props.role}</Card.Text>
+        </h6>
+        <Card.Text>{props.bio}</Card.Text>
+      </Card.Body>
+      <Card.Footer>
+        <small className="text-muted">Commits: </small>
+        <small>{props.commits}</small>
+        <br></br>
+        <small className="text-muted">Issues: </small>
+        <small>{props.issues}</small>
+        <br></br>
+        <small className="text-muted">Unit Tests: </small>
+        <small>{props.unittests}</small>
+      </Card.Footer>
     </Card>
-);
+  );
 }
 
 export default DeveloperCard;
