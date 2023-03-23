1a3a082ff0783da0e4813359b29444f5c3acdb06
Noah Liu <noah.l@utexas.edu>
2023-03-23 11:48:07 -0500

    refactor single endpoints

--- backend/app.py
+++ backend/app.py
@@ -91,90 +91,24 @@ def get_parks():
 
 @app.route("/city/<int:r_id>")
 def get_city(r_id):
-    engine = db.create_engine(db_url)
-    conn = engine.connect()
-    metadata = db.MetaData()
-    cities = db.Table("cities", metadata, autoload=True, autoload_with=engine)
-
-    queryCommnand = "SELECT * FROM cities WHERE id = " + str(r_id + 1)
-    result = conn.execute(queryCommnand).fetchone()
-
-    city = {}
-    city["id"] = result[0]
-    city["airbnb_listings"] = result[1]
-    city["cost"] = result[2]
-    city["hiking_trails"] = result[3]
-    city["latitude"] = result[4]
-    city["long_name"] = result[5]
-    city["longitude"] = result[6]
-    city["name"] = result[7]
-    city["photo"] = result[8]
-    city["population"] = result[9]
-    city["rating"] = result[10]
-    city["safety"] = result[11]
-    city["short_name"] = result[12]
-    city["walkability"] = result[13]
-    city["nearest_airports"] = result[14]
-    city["nearest_parks"] = result[15]
-    return json.dumps(city, indent=4)
+    query = db.session.query(City).filter_by(id=r_id)
+    result = city_schema.dump(query, many=True)[0]
+    return jsonify({"data": result})
 
 
 @app.route("/airport/<int:r_id>")
 def get_airport(r_id):
-    engine = db.create_engine(db_url)
-    conn = engine.connect()
-    metadata = db.MetaData()
-    airports = db.Table("airports", metadata, autoload=True, autoload_with=engine)
-
-    queryCommnand = "SELECT * FROM airports WHERE id = " + str(r_id + 1)
-    row = conn.execute(queryCommnand).fetchone()
-
-    airport = {}
-    airport["id"] = row[0]
-    airport["address"] = row[1]
-    airport["city"] = row[2]
-    airport["iata_code"] = row[3]
-    airport["icao_code"] = row[4]
-    airport["latitude"] = row[5]
-    airport["longitude"] = row[6]
-    airport["name"] = row[7]
-    airport["phone"] = row[8]
-    airport["state"] = row[9]
-    airport["website"] = row[10]
-    airport["zip_code"] = row[11]
-    airport["nearest_cities"] = row[12]
-    airport["nearest_parks"] = row[13]
-    return json.dumps(airport, indent=4)
+    query = db.session.query(Airport).filter_by(id=r_id)
+    result = airport_schema.dump(query, many=True)[0]
+    return jsonify({"data": result})
 
 
 @app.route("/park/<int:r_id>")
 def get_park(r_id):
-    engine = db.create_engine(db_url)
-    conn = engine.connect()
-    metadata = db.MetaData()
-    parks = db.Table("parks", metadata, autoload=True, autoload_with=engine)
+    query = db.session.query(Park).filter_by(id=r_id)
+    result = park_schema.dump(query, many=True)[0]
+    return jsonify({"data": result})
 
-    queryCommnand = "SELECT * FROM parks WHERE id = " + str(r_id + 1)
-    row = conn.execute(queryCommnand).fetchone()
-
-    park = {}
-    park["id"] = row[0]
-    park["activities"] = row[1]
-    park["description"] = row[2]
-    park["email"] = row[3]
-    park["fee"] = row[4]
-    park["latitude"] = row[5]
-    park["longitude"] = row[6]
-    park["name"] = row[7]
-    park["phone"] = row[8]
-    park["photos"] = row[9]
-    park["states"] = row[10]
-    park["topics"] = row[11]
-    park["website"] = row[12]
-    park["weekdays"] = row[13]
-    park["nearest_airports"] = row[14]
-    park["nearest_cities"] = row[15]
-    return json.dumps(park, indent=4)
 
 @app.route("/search/cities/<string:query>")
 def search_cities(query):
