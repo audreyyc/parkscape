6f6156772f4cd0fa8f9369fd2be704d9b1441c48
Petar Iliev <petariliev@utexas.edu>
2023-04-15 12:18:08 -0500

    Refactoring

--- frontend/guitests.py
+++ frontend/guitests.py
@@ -25,7 +25,7 @@ class Test(unittest.TestCase):
         chrome_options.add_argument("--disable-dev-shm-usage")
         chrome_options.add_argument("--no-sandbox")
         chrome_options.add_argument("--headless")
-        self.driver = webdriver.Chrome(executable_path="./chromedriver.exe", chrome_options=chrome_options)
+        self.driver = webdriver.Chrome(chrome_options=chrome_options)
         self.driver.get(URL)
 
     @classmethod
@@ -100,8 +100,8 @@ class Test(unittest.TestCase):
             )
         )
         try:
-            element = self.driver.find_element(By.XPATH,
-                '//*[@id="root"]/div/div[8]/div/div[4]/div/a'
+            element = self.driver.find_element(
+                By.XPATH, '//*[@id="root"]/div/div[8]/div/div[4]/div/a'
             )
             self.driver.execute_script("arguments[0].click();", element)
         except Exception as e:
@@ -116,8 +116,8 @@ class Test(unittest.TestCase):
             )
         )
         try:
-            element = self.driver.find_element(By.XPATH,
-                '//*[@id="root"]/div/div[10]/div/div[3]/div/a'
+            element = self.driver.find_element(
+                By.XPATH, '//*[@id="root"]/div/div[10]/div/div[3]/div/a'
             )
             self.driver.execute_script("arguments[0].click()", element)
         except Exception as e:
@@ -193,7 +193,9 @@ class Test(unittest.TestCase):
         search_bar.send_keys("los angeles")
         search_bar.send_keys(Keys.RETURN)
         time.sleep(5)  # Wait for results to show
-        city = self.driver.find_element(By.XPATH, '/html/body/div/div/div/div[3]/div/div/div/a/div[2]/button')
+        city = self.driver.find_element(
+            By.XPATH, "/html/body/div/div/div/div[3]/div/div/div/a/div[2]/button"
+        )
         city.click()
         self.assertEqual(
             str(self.driver.current_url),
@@ -202,11 +204,7 @@ class Test(unittest.TestCase):
 
     def test_sorting(self):
         self.driver.get(URL + "parks")
-        sorting_select = Select(
-            self.driver.find_element(
-                By.ID, "sort"
-            )
-        )
+        sorting_select = Select(self.driver.find_element(By.ID, "sort"))
         sorting_select.select_by_index(2)
         apply_button = self.driver.find_element(
             By.XPATH, '//*[@id="root"]/div/div/div[2]/div[3]/button'
@@ -220,13 +218,11 @@ class Test(unittest.TestCase):
 
     def test_filtering(self):
         self.driver.get(URL + "cities")
-        filtering_select = Select(
-            self.driver.find_element(
-                By.ID, "states"
-            )
-        )
+        filtering_select = Select(self.driver.find_element(By.ID, "states"))
         filtering_select.select_by_index(10)
-        apply_button = self.driver.find_element(By.XPATH, '//*[@id="root"]/div/div/div[2]/div[3]/button')
+        apply_button = self.driver.find_element(
+            By.XPATH, '//*[@id="root"]/div/div/div[2]/div[3]/button'
+        )
         apply_button.click()
         time.sleep(2)
         first_city_name = self.driver.find_element(
